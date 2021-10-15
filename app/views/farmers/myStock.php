df3cd29a9fe79070544acbc22f4bee6a300422f2
hirushika <hirushikawickramaarachchi@gmail.com>
2021-10-16 00:02:39 +0530

    mystockpage

---
+++ app/views/farmers/myStock.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Boxicons CDN Links-->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/myStockStyle.css" />
+    <link rel="stylesheet" href="myStockstyles.css"/>
+    <title>my stocks</title>
+</head>
+<body>
+    <div class="nav">
+
+    </div>
+    <div class="mystock-content">
+        <div class="left-side">
+            <div class="history">
+            <table class="side-table">
+                    <tr> 
+                        <th> <h2> Stock History</h2> </th>
+                    </tr>  
+                    <tr>
+                        <td> <p> Number of orders Completed : 10 <p> <hr></td> 
+                    </tr> 
+                    <tr>
+                        <td> <p> Total Number of Posts : 6 <p> <hr>  </td>
+                    </tr>
+                    <tr>
+                        <td> <p> Number of Posts Approved : 5 <p> <hr>  </td>
+                    </tr>
+                    <tr>
+                        <td> <p> Number of Posts Rejected : 1 <p> <hr> </td>
+                    </tr>
+                    <tr>
+                        <td> <p> Number of Reports : 0 <p> <hr>  </td>
+                    </tr> 
+                </table>    
+            </div>
+        </div>
+        <div class="right-side">
+            <div class="topic">
+                <h2>My Stocks </h2>
+            </div>
+            <div class="posts-list">
+                <div class="row">
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                    <div class="card">
+                            <div class="stock-img">
+                                <img class="stock-img-1" src="images/carrot.jpg" alt="Stock" style="width:100%">
+                            </div>
+                            <div class="container">
+                                <div class="line-1">
+                                    <div class="text1">
+                                        <p>Carrot 20KG</p>
+                                    </div>
+                                    <div class="text2">
+                                        <p>Rs.3000.00</p>
+                                    </div>
+                                </div>
+                                <div class="line-2">
+                                    <div class="text1">
+                                        <p class="title">Farmer : Nimal</p>
+                                    </div>
+                                    <div class="text2">
+                                        <i class='bx bx-star'></i>
+                                        <i class='bx bx-star'></i>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
