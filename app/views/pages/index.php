48e359a2d98c65e118034b2b8a9ed42f70eccc26
ThilinaVP <thilinavp@gmail.com>
2021-10-22 00:37:01 +0530

    landing page impl

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -35,7 +35,7 @@
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
                 <img src="<?php echo URLROOT; ?>/img/landing1.jpg" style="width:100%">
-                <center><div class="text">
+                <center><div class="text" style="color: black;">
                     <div class="hello" >Hello!</div>
                     <div class="welcome"><strong>Welcome To <br> GOVISEVANA</strong></div>
                     <div class="para"><p >See how your users experience your website in realtime or view <br> trends to see any changes in performance over time.</p></div>
@@ -49,13 +49,22 @@
             <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
                 <img src="<?php echo URLROOT; ?>/img/landing2.jpg" style="width:100%">
-                <div class="text">Caption Two</div>
+                <div class="text" style="color: black;">
+                <div class="hello" >Hello!</div>
+                    <div class="welcome"><strong>Explore <br> The Farmers World</strong></div>
+                    <div class="para"><p >See how your users experience your website in realtime or view <br> trends to see any changes in performance over time.</p></div>
+                    <div class="btn">
+                    <button type="button">SHOP NOW</button>
+                </div>
+                </div>
             </div>
-
+            
             <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
                 <img src="<?php echo URLROOT; ?>/img/landing3.jpg" style="width:100%">
-                <div class="text">Caption Three</div>
+                <div class="text" style="color: black;">
+                <div class="hello" >More products comming Soon!</div>
+                </div>
             </div>
 
         </div>
@@ -110,37 +119,16 @@
                     </div>
                 </div>
 
-                <div class="row-special-list">
-                    <div class="special-grid best-seller">
-                        <div class="products-single fix">
-                            <div class="box-img-hover">
-                                <div class="type-lb">
-                                    <p class="sale">Sale</p>
-                                </div>
-                                <img src="<?php echo URLROOT; ?>/img/img-pro-01.jpg" class="img-fluid" alt="Image">
-                                <div class="mask-icon">
-                                    <ul>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
-                                    </ul>
-                                    <a class="cart" href="#">Add to Cart</a>
-                                </div>
-                            </div>
-                            <div class="why-text">
-                                <h4>Lorem ipsum dolor sit amet</h4>
-                                <h5> $7.79</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="special-grid top-featured">
+                <div class="row-special-list" style="justify-content: center;">
+                    <?php $count = 0; ?>
+                    <?php foreach($data['posts'] as $post) { ?>
+                    <div class="special-grid ">
                         <div class="products-single fix">
                             <div class="box-img-hover">
                                 <div class="type-lb">
                                     <p class="new">New</p>
                                 </div>
-                                <img src="<?php echo URLROOT; ?>/img/img-pro-02.jpg" class="img-fluid" alt="Image">
+                                <img src="<?php echo URLROOT; ?>/img/<?php echo $post -> image ?>" class="img-fluid" alt="Image">
                                 <div class="mask-icon">
                                     <ul>
                                         <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
@@ -151,57 +139,16 @@
                                 </div>
                             </div>
                             <div class="why-text">
-                                <h4>Lorem ipsum dolor sit amet</h4>
-                                <h5> $9.79</h5>
+                                <h4><?php echo $post -> title ?></h4>
+                                <h5> <?php echo $post -> fixedPrice ?> RS</h5>
                             </div>
                         </div>
                     </div>
-
-                    <div class="special-grid top-featured">
-                        <div class="products-single fix">
-                            <div class="box-img-hover">
-                                <div class="type-lb">
-                                    <p class="sale">Sale</p>
-                                </div>
-                                <img src="<?php echo URLROOT; ?>/img/img-pro-03.jpg" class="img-fluid" alt="Image">
-                                <div class="mask-icon">
-                                    <ul>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
-                                    </ul>
-                                    <a class="cart" href="#">Add to Cart</a>
-                                </div>
-                            </div>
-                            <div class="why-text">
-                                <h4>Lorem ipsum dolor sit amet</h4>
-                                <h5> $10.79</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="special-grid best-seller">
-                        <div class="products-single fix">
-                            <div class="box-img-hover">
-                                <div class="type-lb">
-                                    <p class="sale">Sale</p>
-                                </div>
-                                <img src="<?php echo URLROOT; ?>/img/img-pro-04.jpg" class="img-fluid" alt="Image">
-                                <div class="mask-icon">
-                                    <ul>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
-                                        <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
-                                    </ul>
-                                    <a class="cart" href="#">Add to Cart</a>
-                                </div>
-                            </div>
-                            <div class="why-text">
-                                <h4>Lorem ipsum dolor sit amet</h4>
-                                <h5> $15.79</h5>
-                            </div>
-                        </div>
-                    </div>
+                    <?php $count++;
+                    if($count>=4) break;
+                }?>
+                    
+                    
                 </div>
             </div>
         </div>
@@ -211,7 +158,7 @@
     <div class="latest-blog">
         <div class="container">
             <div class="f-row">
-                <div class="col-lg-12"><center>
+                <div class=""><center>
                     <div class="title-all text-center">
                         <h1>Latest Posts</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet lacus enim.</p>
@@ -219,7 +166,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6 col-lg-4 col-xl-4">
+               
                     <div class="blog-box">
                         <div class="blog-img">
                             <img class="img-fluid" src="<?php echo URLROOT; ?>/img/blog-img.jpg" alt="" />
@@ -230,14 +177,13 @@
                                 <p>Nulla ut urna egestas, porta libero id, suscipit orci. Quisque in lectus sit amet urna dignissim feugiat. Mauris molestie egestas pharetra. Ut finibus cursus nunc sed mollis. Praesent laoreet lacinia elit id lobortis.</p>
                             </div>
                             <ul class="option-blog">
-                                <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fas fa-eye"></i></a></li>
-                                <li><a href="#"><i class="far fa-comments"></i></a></li>
+                                <li><a href="#"><i class="">Visit</i></a></li>
+
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
+                
+
                     <div class="blog-box">
                         <div class="blog-img">
                             <img class="img-fluid" src="<?php echo URLROOT; ?>/img/blog-img-01.jpg" alt="" />
@@ -248,14 +194,12 @@
                                 <p>Nulla ut urna egestas, porta libero id, suscipit orci. Quisque in lectus sit amet urna dignissim feugiat. Mauris molestie egestas pharetra. Ut finibus cursus nunc sed mollis. Praesent laoreet lacinia elit id lobortis.</p>
                             </div>
                             <ul class="option-blog">
-                                <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fas fa-eye"></i></a></li>
-                                <li><a href="#"><i class="far fa-comments"></i></a></li>
+                                <li><a href="#">Visit</a></li>
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-4">
+            
+                
                     <div class="blog-box">
                         <div class="blog-img">
                             <img class="img-fluid" src="<?php echo URLROOT; ?>/img/blog-img-02.jpg" alt="" />
@@ -266,13 +210,12 @@
                                 <p>Nulla ut urna egestas, porta libero id, suscipit orci. Quisque in lectus sit amet urna dignissim feugiat. Mauris molestie egestas pharetra. Ut finibus cursus nunc sed mollis. Praesent laoreet lacinia elit id lobortis.</p>
                             </div>
                             <ul class="option-blog">
-                                <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fas fa-eye"></i></a></li>
-                                <li><a href="#"><i class="far fa-comments"></i></a></li>
+                                <li><a href="#"><i class="">Visit</i></a></li>
+
                             </ul>
                         </div>
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
